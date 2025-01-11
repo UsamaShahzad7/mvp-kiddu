709454742cb724bef0c322295baf640d608fd97b
Usama Shahzad <mughalosama618@gmail.com>
2025-01-12 02:46:10 +0500

    Add arcordian content

---
+++ src/app/FAQ.ts
@@ -0,0 +1,27 @@
+export const instructions = [
+  {
+    title: "Friendly Introduction",
+    subTitle:
+      "This app is like a fun playground where you can learn new things and play cool quizzes about animals, numbers, shapes, and more! You’ll have fun while learning. Play again and again to get even better!",
+  },
+  {
+    title: "How to Move Around",
+    subTitle:
+      "Every time you see buttons or arrows, you can tap them to move forward or go back. It’s like a treasure hunt—just tap to go to the next fun thing!",
+  },
+  {
+    title: "How to Start a Quiz",
+    subTitle:
+      "You can pick a quiz or game about animals, numbers, or shapes. There are pictures to help you choose what looks fun!",
+  },
+  {
+    title: "How to Answer Questions",
+    subTitle:
+      "Some questions show pictures you can tap. For others, you’ll have to press the right button to answer. Give it your best try! When you get the answer right, you’ll see fireworks! If you make a mistake, that’s okay—you can try again!",
+  },
+  {
+    title: "Rewards and Progress",
+    subTitle:
+      "Every time you answer a question or finish a game, you get a badge. The more badges you collect, the better you’re doing! At the end of each game, you’ll see how well you did.",
+  },
+];
